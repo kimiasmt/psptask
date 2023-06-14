b27ed4deaadb179952389cce3df74af32ab8110c
kimia <kimia.smt@gmail.com>
2023-06-14 12:41:00 +0330

    rebase with develop

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {CustomButton} from "../CustomButton/CustomButton";
+import {router} from "next/client";
+import {useRouter} from "next/router";
 // import './menu.scss'
 const Menu = () => {
+    const router = useRouter();
+    const navigateToLink = (url : string):void => {
+        router.push(url)
+    }
+
     return(
         <div className={'flex flex-row justify-around content-center w-1/2 bg-blue-100 rounded-lg'}>
-            <CustomButton name={'setting'} action={() => console.log('setting')}/>
-            <CustomButton name={'list'} action={() => console.log('setting')}/>
+            <CustomButton name={'setting'} action={() => navigateToLink('setting')}/>
+            <CustomButton name={'list'} action={() => navigateToLink('list')}/>
         </div>
     )
 }
--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import {useQuery} from "react-query";
+import {BASE_URL} from "../../public/EnvironmentUrl";
 const ListPage = () => {
+    const [commentList,setComment] = useState<any[]>([])
+    const { data, isLoading, error } = useQuery('List', async () => {
+        const response = await fetch(`${BASE_URL}/comments`);
+        return console.log(response.json().then(data => data));
+    });
     return(
         <div>
         kimia test
